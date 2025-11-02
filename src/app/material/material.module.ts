4d334d7cf37db7cbc25fcb1d82793412ffedb853
Xanaks15 <josue.torresz@alumno.buap.mx>
2025-11-02 03:41:44 -0600

    matSide

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   exports: [
-    
+    MatSidenavModule,
   ]
 })
 export class MaterialModule { }
